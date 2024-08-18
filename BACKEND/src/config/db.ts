1a50088f1f9c4e8692708382eb367417a2d7f85d
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-08-18 17:24:50 -0400

    chore: remove import from db config

--- BACKEND/src/config/db.ts
+++ BACKEND/src/config/db.ts
@@ -1,16 +1,47 @@
+import dotenv from "dotenv";
 import pkg from "pg";
-const { Pool } = pkg;
-import { config } from "./env.js";
+
+dotenv.config();
+
+function getEnvVariable(name: string, defaultValue: string = ""): string {
+  const value = process.env[name];
+  return value !== undefined ? value : defaultValue;
+}
+
+function getEnvVariableAsNumber(name: string, defaultValue: number = 0): number {
+  const value = process.env[name];
+  return value !== undefined ? Number(value) : defaultValue;
+}
+
+export const config = {
+  // Local DB configuration
+  dbNameLocal: getEnvVariable("DB_NAME_LOCAL"),
+  dbUserLocal: getEnvVariable("DB_USER_LOCAL"),
+  dbPasswordLocal: getEnvVariable("DB_PASSWORD_LOCAL"),
+  dbHostLocal: getEnvVariable("DB_HOST_LOCAL"),
+  dbPortLocal: getEnvVariableAsNumber("DB_PORT_LOCAL", 5432),
+
+  // Remote DB configuration
+  dbNameRemote: getEnvVariable("DB_NAME_REMOTE"),
+  dbUserRemote: getEnvVariable("DB_USER_REMOTE"),
+  dbPasswordRemote: getEnvVariable("DB_PASSWORD_REMOTE"),
+  dbHostRemote: getEnvVariable("DB_HOST_REMOTE"),
+  dbPortRemote: getEnvVariableAsNumber("DB_PORT_REMOTE", 5432),
+
+  // Other settings
+  port: getEnvVariableAsNumber("PORT", 3000),
+  useRemoteDb: process.env.USE_REMOTE_DB === "true",
+};
 
 console.log(config);
+
+const { Pool } = pkg;
 
 const pool = new Pool({
   user: config.useRemoteDb ? config.dbUserRemote : config.dbUserLocal,
   host: config.useRemoteDb ? config.dbHostRemote : config.dbHostLocal,
   database: config.useRemoteDb ? config.dbNameRemote : config.dbNameLocal,
-  password: config.useRemoteDb
-    ? config.dbPasswordRemote
-    : config.dbPasswordLocal,
+  password: config.useRemoteDb ? config.dbPasswordRemote : config.dbPasswordLocal,
   port: config.useRemoteDb ? config.dbPortRemote : config.dbPortLocal,
   ssl: config.useRemoteDb ? { rejectUnauthorized: false } : undefined,
 });
