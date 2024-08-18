0c48607748b51b9cddf1a533b4f9275e29e491d7
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-08-18 17:05:13 -0400

    chore: modify configuration env.ts and db.ts

--- BACKEND/src/config/db.ts
+++ BACKEND/src/config/db.ts
@@ -1,14 +1,16 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 import { config } from "./env.js";
 
 const pool = new Pool({
-  user: config.useRemoteDb ? config.dbUserRemote : config.dbUserLocal,
-  host: config.useRemoteDb ? config.dbHostRemote : config.dbHostLocal,
-  database: config.useRemoteDb ? config.dbNameRemote : config.dbNameLocal,
-  password: config.useRemoteDb ? config.dbPasswordRemote : config.dbPasswordLocal,
-  port: config.useRemoteDb ? Number(config.dbPortRemote) : Number(config.dbPortLocal),
-  ssl: config.useRemoteDb ? { rejectUnauthorized: false } : undefined,
+  user: config.useRemoteDb() ? config.dbUserRemote() : config.dbUserLocal(),
+  host: config.useRemoteDb() ? config.dbHostRemote() : config.dbHostLocal(),
+  database: config.useRemoteDb() ? config.dbNameRemote() : config.dbNameLocal(),
+  password: config.useRemoteDb()
+    ? config.dbPasswordRemote()
+    : config.dbPasswordLocal(),
+  port: config.useRemoteDb() ? config.dbPortRemote() : config.dbPortLocal(),
+  ssl: config.useRemoteDb() ? { rejectUnauthorized: false } : undefined,
 });
 
 export default pool;
