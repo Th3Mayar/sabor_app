9ca83a810e666f5edcd59428b1207d655e20ce59
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-08-18 17:20:02 -0400

    chore: change env.ts file to resolve error deploy

--- BACKEND/src/config/env.ts
+++ BACKEND/src/config/env.ts
@@ -2,40 +2,32 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+function getEnvVariable(name: string, defaultValue: string = ""): string {
+  const value = process.env[name];
+  return value !== undefined ? value : defaultValue;
+}
+
+function getEnvVariableAsNumber(name: string, defaultValue: number = 0): number {
+  const value = process.env[name];
+  return value !== undefined ? Number(value) : defaultValue;
+}
+
 export const config = {
   // Local DB configuration
-  dbNameLocal:
-    process.env.DB_NAME_LOCAL !== undefined ? process.env.DB_NAME_LOCAL : "",
-  dbUserLocal:
-    process.env.DB_USER_LOCAL !== undefined ? process.env.DB_USER_LOCAL : "",
-  dbPasswordLocal:
-    process.env.DB_PASSWORD_LOCAL !== undefined
-      ? process.env.DB_PASSWORD_LOCAL
-      : "",
-  dbHostLocal:
-    process.env.DB_HOST_LOCAL !== undefined ? process.env.DB_HOST_LOCAL : "",
-  dbPortLocal:
-    process.env.DB_PORT_LOCAL !== undefined
-      ? Number(process.env.DB_PORT_LOCAL)
-      : 5432,
+  dbNameLocal: getEnvVariable("DB_NAME_LOCAL"),
+  dbUserLocal: getEnvVariable("DB_USER_LOCAL"),
+  dbPasswordLocal: getEnvVariable("DB_PASSWORD_LOCAL"),
+  dbHostLocal: getEnvVariable("DB_HOST_LOCAL"),
+  dbPortLocal: getEnvVariableAsNumber("DB_PORT_LOCAL", 5432),
 
   // Remote DB configuration
-  dbNameRemote:
-    process.env.DB_NAME_REMOTE !== undefined ? process.env.DB_NAME_REMOTE : "",
-  dbUserRemote:
-    process.env.DB_USER_REMOTE !== undefined ? process.env.DB_USER_REMOTE : "",
-  dbPasswordRemote:
-    process.env.DB_PASSWORD_REMOTE !== undefined
-      ? process.env.DB_PASSWORD_REMOTE
-      : "",
-  dbHostRemote:
-    process.env.DB_HOST_REMOTE !== undefined ? process.env.DB_HOST_REMOTE : "",
-  dbPortRemote:
-    process.env.DB_PORT_REMOTE !== undefined
-      ? Number(process.env.DB_PORT_REMOTE)
-      : 5432,
+  dbNameRemote: getEnvVariable("DB_NAME_REMOTE"),
+  dbUserRemote: getEnvVariable("DB_USER_REMOTE"),
+  dbPasswordRemote: getEnvVariable("DB_PASSWORD_REMOTE"),
+  dbHostRemote: getEnvVariable("DB_HOST_REMOTE"),
+  dbPortRemote: getEnvVariableAsNumber("DB_PORT_REMOTE", 5432),
 
   // Other settings
-  port: process.env.PORT !== undefined ? Number(process.env.PORT) : 3000,
+  port: getEnvVariableAsNumber("PORT", 3000),
   useRemoteDb: process.env.USE_REMOTE_DB === "true",
 };
